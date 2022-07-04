2b427eeed26d0029b677c7ace1ce45e49404807c
afsanehkh <you@example.com>
2022-07-03 20:45:28 -0700

    Improve

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -15,9 +15,7 @@ const cartReducer = (state, action) => {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     )
-
     const existingCartItem = state.items[existingCartItemIndex]
-
     let updatedItems
 
     if (existingCartItem) {
@@ -30,12 +28,12 @@ const cartReducer = (state, action) => {
     } else {
       updatedItems = state.items.concat(action.item)
     }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     }
   }
-
   if (action.type === 'REMOVE') {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
@@ -50,11 +48,17 @@ const cartReducer = (state, action) => {
       updatedItems = [...state.items]
       updatedItems[existingCartItemIndex] = updatedItem
     }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     }
   }
+
+  if (action.type === 'CLEAR') {
+    return defaultCartState
+  }
+
   return defaultCartState
 }
 
@@ -72,11 +76,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'REMOVE', id: id })
   }
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: 'CLEAR' })
+  }
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   }
 
   return (
